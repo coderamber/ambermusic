74307e33bb9353479377ef01739d31e2b604fff5
coderamber <605968453@qq.com>
2022-09-08 23:55:08 +0800

    ci(assets): add some pictures and reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-function App() {
+import React, { memo } from 'react'
+
+const App = memo(() => {
   return (
     <div>
-      首页
+      <h2>App</h2>
     </div>
-  );
-}
+  )
+})
 
-export default App;
+export default App
