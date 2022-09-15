6a9bf75a1cc379af2aa99f49bd5cc45252902314
coderamber <605968453@qq.com>
2022-09-15 21:40:16 +0800

    pref(store): use immutable to improve performance

--- src/pages/discover/c-pages/recommend/store/reducer.ts
+++ src/pages/discover/c-pages/recommend/store/reducer.ts
@@ -1,13 +1,14 @@
+import { Map } from "immutable";
 import * as ActionTypes from './constants'
 
-const defaultState = {
+const defaultState = Map({
   topBanners: []
-}
+})
 
 function reducer(state = defaultState, action: any) {
   switch (action.type) {
     case ActionTypes.CHANGE_TOP_BANNERS:
-      return { ...state, topBanners: action.topBanners };
+      return state.set("topBanners", action.topBanners)
     default:
       return state;
   }
